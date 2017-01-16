e5c09ef818723404b501aa5d97c63df382510c83
Mikkel Wraae Hansen <mikkel@wraae-hansen.dk>
2017-01-16 22:09:08 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,7 @@ class ProfilesController < ApplicationController
     # GET to /users/:user_id/profiles/new
     def new 
         # Render blank profile form
+        @profile = Profile.new
     end
+    
 end
